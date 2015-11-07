7695fd058459b7c4c410bdd5756af88dbc144d7d
mlinnem <micah.linnemeier@gmail.com>
2015-11-07 16:25:10 -0600

    Various things. Added nifty animations. Kids bounce off walls now. Refactoring.

--- witch_game_tile_tween.js
+++ witch_game_tile_tween.js
@@ -28,16 +28,25 @@ const DIRECTION_NW = new Phaser.Point(-1, -1);
 var kid;
 var kidState;
 
-var NORMAL_MOVE_SPEED = 15;
-var SCARED_MOVE_SPEED = 35;
+var scareCountdown;
+//TODO: probably shouldn't be global
+var breatheIn;
+var breatheOut;
+var finalBreatheIn;
+var finalBreatheOut;
 
-const STATE_NORMAL = 0;
+//Tiles traversed in a second
+var NORMAL_MOVE_SPEED = .5;
+var SCARED_MOVE_SPEED = 1.2;
+
+const STATE_WANDERING = 0;
 const STATE_SCAREDSEEING = 5;
 const STATE_SCAREDRUNNING = 1;
 const STATE_SIGNSEEING = 6;
 const STATE_SIGNAPPROACHING = 2;
 const STATE_SIGNREADING = 3;
 const STATE_SIGNFOLLOWING = 4;
+const STATE_
 
 var kidDirection;
 
@@ -62,17 +71,16 @@ function create() {
     game.time.create();
 
     //KID SETUP
-    kid = createKid(new Phaser.Point(0,0));
+    kid = createKid(new Phaser.Point(28,10));
 
     hauntedtrees = game.add.group();
 
     game.input.onDown.add(addHauntedTree, this);
 
     cursors = game.input.keyboard.createCursorKeys();
-    hauntedtreecursor = game.add.sprite(0, 0, 'hauntedtree');
+    hauntedtreecursor = game.add.sprite(25, 25, 'hauntedtree');
     hauntedtreecursor.alpha = .3;
     cursorsprite = hauntedtreecursor;
-
     continueOnKid(kid);
 }
 
@@ -80,8 +88,9 @@ function addHauntedTree() {
     var cursorGridPosX = getGridPosSingle(game.input.mousePointer.x);
     var cursorGridPosY = getGridPosSingle(game.input.mousePointer.y);
 
-    var spritePixelTopLeft = locationToPixelTopLeft(new Phaser.Point(cursorGridPosX, cursorGridPosY));
     var hauntedtree = hauntedtrees.create(0, 0, 'hauntedtree');
+    hauntedtree.anchor.x = .5;
+    hauntedtree.anchor.y = .5;
     setGridLocation(hauntedtree, new Phaser.Point(cursorGridPosX, cursorGridPosY));
     hauntedtree.name = 'hauntedtree';
 
@@ -100,19 +109,130 @@ function scare(context, kidforsomereason, timeforsomereason, visitingKid, haunte
       var kidRelativeToTree = Phaser.Point.subtract(kidGridLocation(kid), getGridPosOfObject(hauntedTree));
       var newDirection = kidRelativeToTree;
       kidDirection = newDirection;
-      moveTo(kid, Phaser.Point.add(kidGridLocation(kid),kidDirection));
-      //prevents other lower priority actions from taking effect
+      kidBecomeScared(kid);
+        //prevents other lower priority actions from taking effect
       //TODO: Handle simultaneous scare corner case (overlapping scare areas of two trees)
       visitedTile.onVisit.halt();
 }
 
+function kidBecomeScared(kid) {
+
+
+  kidState = STATE_SCAREDSEEING;
+
+  kid.frameName = 'ScaredKid.png';
+  var jumpUp = game.add.tween(kid.scale);
+  var jumpDown = game.add.tween(kid.scale);
+  jumpUp.to({x: .35, y: .35}, 130, Phaser.Easing.Linear.None);
+  jumpDown.to({x: .25, y: .25}, 130, Phaser.Easing.Linear.None);
+  jumpUp.chain(jumpDown);
+
+  jumpDown.onComplete.add(function() {
+      kidState = STATE_SCAREDRUNNING;
+
+
+
+      breatheHeavily();
+      scareCountdown = 3;
+      continueOnKid(kid);
+      }
+  );
+
+  jumpUp.start();
+}
+
+function breatheHeavily() {
+  //breathe heavily
+  breatheIn = game.add.tween(kid.scale);
+  breatheOut = game.add.tween(kid.scale);
+  breatheIn.to({x: .30, y: .30}, 320, Phaser.Easing.Sinusoidal.In);
+ breatheOut.to({x: .25, y: .25},320, Phaser.Easing.Sinusoidal.Out);
+  breatheIn.chain(breatheOut);
+  breatheOut.onComplete.add(breatheHeavily);
+  breatheIn.start();
+  log("Breathing heavily");
+}
+
+function stopBreathingHeavily() {
+  breatheIn.stop(false);
+  breatheOut.stop(false);
+  //final exhale
+  finalBreatheIn = game.add.tween(kid.scale);
+  finalBreatheOut = game.add.tween(kid.scale);
+  finalBreatheIn.to({x: .29, y: .29}, 800, Phaser.Easing.Sinusoidal.In);
+  finalBreatheOut.to({x: .25, y: .25}, 1300, Phaser.Easing.Sinusoidal.Out);
+  finalBreatheIn.chain(finalBreatheOut);
+  finalBreatheIn.start();
+  log("stoppin");
+}
+
+function kidStopScared(kid) {
+  stopBreathingHeavily();
+  kidState = STATE_WANDERING;
+  kid.frameName = "NormalKid.png";
+}
+
+function considerHowToDealWithWall(kid, whereToMove) {
+  lookConsidering(kid);
+
+  game.time.events.add(600, function() {
+    decideOnDirectionAndMove(kid, whereToMove);
+   },
+    this);
+}
+
+function lookConsidering(kid) {
+    var consider = game.add.tween(kid.scale);
+    consider.to({x: .25, y: .22}, 80, Phaser.Easing.Linear.None);
+    consider.start();
+}
+
+function lookDecidedOnDirection() {
+    var stopConsidering = game.add.tween(kid.scale);
+    stopConsidering.to({x: .25, y: .25}, 100, Phaser.Easing.Linear.In);
+    stopConsidering.start();
+}
+
+function decideOnDirectionAndMove(kid, whereToMove) {
+    lookDecidedOnDirection();
+    kidDirection = calculateDirectionOffEdge(kid, whereToMove);
+    var newWhereToMove = Phaser.Point.add(kidGridLocation(kid), kidDirection);
+    moveTo(kid, newWhereToMove, NORMAL_MOVE_SPEED);
+}
+
 function continueOn(the_this, visitingKid, visitedTile) {
-  //Will redirect to that speciic kid's continue on function if this is ever object oriented
+  //Will redirect to that specific kid's continue on function if this is ever object oriented
   continueOnKid(visitingKid);
 }
 
 function continueOnKid(kid) {
-moveTo(kid, Phaser.Point.add(kidGridLocation(kid), kidDirection));
+  //TODO: is this right place for this?
+  if (kidState == STATE_SCAREDRUNNING && scareCountdown == 0) {
+    kidStopScared(kid);
+  }
+
+  switch(kidState) {
+    case STATE_WANDERING:
+      var whereToMove = Phaser.Point.add(kidGridLocation(kid), kidDirection);
+      if (willHitEdge(kid, whereToMove)) {
+        considerHowToDealWithWall(kid, whereToMove);
+      } else {
+        moveTo(kid, whereToMove, NORMAL_MOVE_SPEED);
+      }
+      break;
+    case STATE_SCAREDRUNNING:
+      scareCountdown -= 1;
+      var whereToMove = Phaser.Point.add(kidGridLocation(kid), kidDirection);
+      if (willHitEdge(kid, whereToMove)) {
+        kidDirection = calculateDirectionOffEdge(kid, whereToMove);
+        var newWhereToMove = Phaser.Point.add(kidGridLocation(kid), kidDirection);
+        moveTo(kid, newWhereToMove, SCARED_MOVE_SPEED);
+      } else {
+        moveTo(kid, whereToMove, SCARED_MOVE_SPEED);
+      }
+      break;
+  }
+
 }
 
 function kidGridLocation(kid) {
@@ -135,18 +255,21 @@ function getSurroundingTiles(distance, position) {
 function createKid(location) {
   var kid = game.add.sprite(0, 0, 'kid_spritesheet', 'NormalKid.png');
   kid.scale.setTo(.25, .25); //TODO: scale down in the export, since this leads to fuzziness
-  kidState = "Wandering";
+  kidState = STATE_WANDERING;
   kidDirection = DIRECTION_SE;
   setGridLocation(kid, location);
+  kid.anchor.x = 0.5;
+  kid.anchor.y = 0.5;
   return kid;
 }
 
 function setGridLocation(thing, gridLocation) {
-  var pixelLocation = locationToPixelTopLeft(gridLocation);
+  var pixelLocation = locationToPixelCenter(gridLocation);
   thing.x = pixelLocation.x;
   thing.y = pixelLocation.y;
   //thing.gridLocation = gridLocation;
 }
+
 
 function locationToPixelCenter(location) {
   return new Phaser.Point(location.x * TILE_SIZE + TILE_SIZE / 2, location.y * TILE_SIZE + TILE_SIZE / 2);
@@ -157,7 +280,7 @@ function locationToPixelTopLeft(location) {
 }
 
 function spriteCenter(tile) {
-  return new Phaser.Point(tile.x + TILE_SIZE / 2, tile.y + TILE_SIZE / 2);
+  return new Phaser.Point(tile.x, tile.y);
 }
 
 
@@ -166,14 +289,29 @@ function update() {
   cursorsprite.y = lockToGrid(game.input.mousePointer.y);
 }
 
-function moveTo(kid, gridLoc) {
+function moveTo(kid, gridLoc, speed) {
   var tile = getTILE(gridLoc);
   var move = game.add.tween(kid);
-  var kidCenterOffset = TILE_SIZE / 2; //Need to offset since we're moving x and y, not kid center to tile
-  move.to({x: spriteCenter(tile).x - kidCenterOffset, y: spriteCenter(tile).y - kidCenterOffset}, 2000, Phaser.Easing.Linear.None);
+  move.to({x: locationToPixelCenter(gridLoc).x, y: locationToPixelCenter(gridLoc).y}, 1000 / speed, Phaser.Easing.Linear.None);
   move.onComplete.addOnce(kidReactToSurroundings, this, kid, tile);
   move.start();
   log("kid moving to " + getGridPosOfObject(tile).x + ", " + getGridPosOfObject(tile).y);
+}
+
+function calculateDirectionOffEdge(kid, movingTo) {
+  var flipEW = 1, flipNS = 1;
+  if (movingTo.x >= BOARD_COLS || movingTo.x < 0) {
+    flipEW = -1;
+  }
+  if (movingTo.y >= BOARD_ROWS || movingTo.y < 0) {
+    flipNS = -1;
+  }
+  var newDirection = new Phaser.Point(kidDirection.x * flipEW, kidDirection.y * flipNS);
+  return newDirection;
+}
+
+function willHitEdge(kid, movingTo) {
+  return movingTo.x >= BOARD_COLS || movingTo.y >= BOARD_ROWS || movingTo.x < 0 || movingTo.y < 0;
 }
 
 function render() {
@@ -199,9 +337,6 @@ function lockToGrid(coord) {
     return coord - (coord % TILE_SIZE);
 }
 
-function checkKidMoved(kid) {
-
-}
 
 // fill the screen with as many TILEs as possible
 function spawnBoard() {
@@ -215,10 +350,12 @@ function spawnBoard() {
     {
         for (var j = 0; j < BOARD_ROWS; j++)
         {
-            var TILE = TILEs.create(i * TILE_SIZE_SPACED, j * TILE_SIZE_SPACED, "TILES");
+            var TILE = TILEs.create(0, 0, "TILES");
             TILE.name = 'TILE' + i.toString() + 'x' + j.toString();
             TILE.inputEnabled = true;
             TILE.onVisit = new Phaser.Signal();
+            TILE.anchor.x = .5;
+            TILE.anchor.y = .5;
             //If no other action takes precedence, kid continues on from the tile by default.
             TILE.onVisit.add(continueOn, this, -1, TILE);
             //TILE.events.onInputDown.add(selectTILE, this);
@@ -255,7 +392,7 @@ function getGridPosOfObject(object) {
 
 // set the position on the board for a TILE
 function setTILEPos(TILE, posX, posY) {
-
+    setGridLocation(TILE, new Phaser.Point(posX, posY));
     TILE.posX = posX;
     TILE.posY = posY;
     TILE.id = calcTILEId(posX, posY);
